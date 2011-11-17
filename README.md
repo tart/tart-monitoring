3f62f76303d6e354f6a4754b02624ca394fe3bce
Emre Hasegeli <hasegeli@tart.com.tr>
2011-11-17 12:36:18 +0200

    Parameters added for the limits.

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-CheckMySQLProcesslist a script to monitor MySQL processlist
-
-## Usage
-
-./checkMySQLProcesslist.sh [-H hostname] [-P port] [-u username] [-p password]
+CheckMySQLProcesslist a script to monitor MySQL processlist.
 
 ## Description
 
@@ -11,96 +7,62 @@ Executes just one "show processlist" query on the server. Parse the output.
 Gives
 
 * all of the following values as performance data,
-* notifications if the following limits exceeded,
+* notifications if the limits for the following exceeded,
 * longest query information if it is running for 1 minute.
 
-Exit with
+### Variable List
 
-* 0 for ok,
-* 1 for warning,
-* 2 for critical,
-* 3 for unknown.
+* All connections
+* All Queries
+* Queries running more than 10 seconds
+* Queries running more than a minute
+* Queries running more than 10 minutes
+* Queries running more than an hour
+* Longest query time
+* Sleeping connections
+* Querying connections
+* Connecting connections
+* Quitting connections
+* Preparing connections
+* Fetching connections
+* Executing connections
+* Delayed connections
 
-<table >
-	<tr >
-		<th >Counted Process'</th>
-		<th >Warning Limit</th>
-		<th >Critical Limit</th>
-	</tr>
-	<tr >
-		<td >All</td>
-		<td >144</td>
-		<td >377</td>
-	</tr>
-	<tr >
-		<td >Queries</td>
-		<td >21</td>
-		<td >55</td>
-	</tr>
-	<tr >
-		<td >Queries running more than 10 seconds</td>
-		<td >8</td>
-		<td >21</td>
-	</tr>
-	<tr >
-		<td >Queries running more than a minute</td>
-		<td >3</td>
-		<td >8</td>
-	</tr>
-	<tr >
-		<td >Queries running more than 10 minutes</td>
-		<td >1</td>
-		<td >3</td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Queries running more than an hour</td>
-		<td ></td>
-		<td >1</td>
-	</tr>
-	<tr >
-		<td >Longest query time</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Sleeping</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Querying</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Connecting</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Quitting</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Preparing</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Fetching</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Executing</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-	<tr >
-		<td >Delayed</td>
-		<td ></td>
-		<td ></td>
-	</tr>
-</table>
+### Exit Status
+
+* 0 for ok
+* 1 for warning
+* 2 for critical
+* 3 for unknown
+
+## Usage
+
+./checkMySQLProcesslist.sh -h
+
+./checkMySQLProcesslist.sh [-H hostname] [-P port] [-u username] [-p password] \
+		[-c limits] [-w limits]
+
+Hostname:
+
+Hostname of the host.
+
+Port:
+
+Post of the MySQL server.
+
+Username:
+
+Username for the MySQL server.
+
+Password:
+
+Password for the MySQL server.
+
+Limits:
+
+Comma separated critical, warning limits. Written as c1,c2,c3... for critical,
+as w1,w2,w3... for warning ordered by variable list given above.
+
+### Example
+
+./checkMySQLProcesslist.sh -u *** -p *** -c 377,55,21,8,3,1 -w 144,21,8,3
--- checkMySQLProcesslist.sh
+++ checkMySQLProcesslist.sh
@@ -10,18 +10,19 @@
 
 echo -n "CheckMySQLProcesslist "
 
-while getopts "H:P:u:p:h" opt
+while getopts "H:P:u:p:c:w:h" opt
 	do
 	case $opt in
 		H )	connectionString=$connectionString"--host=$OPTARG " ;;
 		P )	connectionString=$connectionString"--port $OPTARG " ;;
 		u )	connectionString=$connectionString"--user=$OPTARG " ;;
 		p )	connectionString=$connectionString"--password=$OPTARG " ;;
+		c )	criticalLimits=$OPTARG ;;
+		w )	warningLimits=$OPTARG ;;
 		h )	echo "a script to monitor MySQL processlist"
-			echo "Usage:"
-			echo "$0 [-h] [-H hostname] [-P port] [-u username] [-p password]"
-			echo "Source:"
-			echo "github.com/tart/CheckMySQLProcesslist"
+			echo "Usage: $0 [-h] [-H hostname] [-P port] [-u username] [-p password] \\"
+			echo "		[-c limits] [-w limits]"
+			echo "Source: github.com/tart/CheckMySQLProcesslist"
 			exit 3 ;;
 		\? )	echo "unknown: wrong parameter"
 			exit 3
@@ -36,20 +37,22 @@ if [ ! "$processlist" ]
 fi
 
 total=$(echo "$processlist" | wc -l)
-if [ $total -ge 377 ]
+totalCriticalLimit=$(echo "$criticalLimits" | cut -d , -f 1)
+totalWarningLimit=$(echo "$warningLimits" | cut -d , -f 1)
+if [ "$totalCriticalLimit" ] && [ $total -ge $totalCriticalLimit ]
 	then
-	criticalString=$criticalString"connections are $total reached 377; "
-elif [ $total -ge 144 ]
+	criticalString=$criticalString"connections are $total reached $totalCriticalLimit; "
+elif [ "$totalWarningLimit" ] && [ $total -ge $totalWarningLimit ]
 	then
-	warningString=$warningString"connections are $total reached 144; "
+	warningString=$warningString"connections are $total reached $totalWarningLimit; "
 fi
-performanceData=$performanceData"connections=$total;144;377 "
+performanceData=$performanceData"connections=$total;$totalWarningLimit;$totalCriticalLimit "
 
 totalQueries=0
-queriesRunningForAnHour=0
-queriesRunningFor10Minutes=0
-queriesRunningForAMinute=0
-queriesRunningFor10Seconds=0
+queriesRunningFor3600=0
+queriesRunningFor600=0
+queriesRunningFor60=0
+queriesRunningFor10=0
 longestQueryTime=0
 queryProcesslist=$(echo "$processlist" | sed "/^[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\tNULL/d")
 for queryTime in $(echo "$queryProcesslist" | cut -f 6)
@@ -57,16 +60,16 @@ for queryTime in $(echo "$queryProcesslist" | cut -f 6)
 	totalQueries=$(expr $totalQueries + 1)
 	if [ $queryTime -gt 10 ]
 		then
-		queriesRunningFor10Seconds=$(expr $queriesRunningFor10Seconds + 1)
+		queriesRunningFor10=$(expr $queriesRunningFor10 + 1)
 		if [ $queryTime -gt 60 ]
 			then
-			queriesRunningForAMinute=$(expr $queriesRunningForAMinute + 1)
+			queriesRunningFor60=$(expr $queriesRunningFor60 + 1)
 			if [ $queryTime -gt 600 ]
 				then
-				queriesRunningFor10Minutes=$(expr $queriesRunningFor10Minutes + 1)
+				queriesRunningFor600=$(expr $queriesRunningFor600 + 1)
 				if [ $queryTime -gt 3600 ]
 					then
-					queriesRunningForAnHour=$(expr $queriesRunningForAnHour + 1)
+					queriesRunningFor3600=$(expr $queriesRunningFor3600 + 1)
 				fi
 			fi
 			if [ $queryTime -gt $longestQueryTime ]
@@ -77,47 +80,60 @@ for queryTime in $(echo "$queryProcesslist" | cut -f 6)
 	fi
 done
 
-if [ $totalQueries -ge 55 ]
+totalQueriesCriticalLimit=$(echo "$criticalLimits" | cut -d , -f 2 -s)
+totalQueriesWarningLimit=$(echo "$warningLimits" | cut -d , -f 2 -s)
+if [ "$totalCriticalLimit" ] && [ $totalQueries -ge $totalQueriesCriticalLimit ]
 	then
-	criticalString=$criticalString"$totalQueries queries reached 55; "
-elif [ $totalQueries -ge 21 ]
+	criticalString=$criticalString"$totalQueries queries reached $totalQueriesCriticalLimit; "
+elif [ "$totalQueriesWarningLimit" ] && [ $totalQueries -ge $totalQueriesWarningLimit ]
 	then
-	warningString=$warningString"$totalQueries queries reached 21; "
+	warningString=$warningString"$totalQueries queries reached $totalQueriesWarningLimit; "
 fi
-performanceData=$performanceData"totalQueries=$totalQueries;21;55 "
+performanceData=$performanceData"totalQueries=$totalQueries;$totalQueriesWarningLimit;$totalQueriesCriticalLimit "
 
-if [ $queriesRunningFor10Seconds -ge 21 ]
+queriesRunningFor10CriticalLimit=$(echo "$criticalLimits" | cut -d , -f 3 -s)
+queriesRunningFor10WarningLimit=$(echo "$warningLimits" | cut -d , -f 3 -s)
+if [ "$queriesRunningFor10CriticalLimit" ] && [ $queriesRunningFor10 -ge $queriesRunningFor10CriticalLimit ]
 	then
-	criticalString=$criticalString"$queriesRunningFor10Seconds queries running for 10 seconds reached 21; "
-elif [ $queriesRunningFor10Seconds -ge 8 ]
+	criticalString=$criticalString"$queriesRunningFor10CriticalLimit queries running for 10 seconds reached $queriesRunningFor10CriticalLimit; "
+elif [ "$queriesRunningFor10WarningLimit" ] && [ $queriesRunningFor10 -ge $queriesRunningFor10WarningLimit ]
 	then
-	warningString=$warningString"$queriesRunningFor10Seconds queries running for 10 seconds reached 8; "
+	warningString=$warningString"$queriesRunningFor10 queries running for 10 seconds reached $queriesRunningFor10WarningLimit; "
 fi
-performanceData=$performanceData"queriesRunningFor10Seconds=$queriesRunningFor10Seconds;8;21 "
+performanceData=$performanceData"queriesRunningFor10Seconds=$queriesRunningFor10;$queriesRunningFor10WarningLimit;$queriesRunningFor10CriticalLimit "
 
-if [ $queriesRunningForAMinute -ge 8 ]
+queriesRunningFor60CriticalLimit=$(echo "$criticalLimits" | cut -d , -f 4 -s)
+queriesRunningFor60WarningLimit=$(echo "$warningLimits" | cut -d , -f 4 -s)
+if [ "$queriesRunningFor60CriticalLimit" ] && [ $queriesRunningFor60 -ge $queriesRunningFor60CriticalLimit ]
 	then
-	criticalString=$criticalString"$queriesRunningForAMinute queries running for a minute reached 8; "
-elif [ $queriesRunningForAMinute -ge 3 ]
+	criticalString=$criticalString"$queriesRunningFor60 queries running for a minute reached $queriesRunningFor60CriticalLimit; "
+elif [ "$queriesRunningFor60WarningLimit" ] && [ $queriesRunningFor60 -ge $queriesRunningFor60WarningLimit ]
 	then
-	warningString=$warningString"$queriesRunningForAMinute queries running for a minute reached 3; "
+	warningString=$warningString"$queriesRunningFor60 queries running for a minute reached $queriesRunningFor60WarningLimit; "
 fi
-performanceData=$performanceData"queriesRunningForAMinute=$queriesRunningForAMinute;3;8 "
+performanceData=$performanceData"queriesRunningFor60=$queriesRunningFor60;$queriesRunningFor60WarningLimit;$queriesRunningFor60CriticalLimit "
 
-if [ $queriesRunningFor10Minutes -ge 3 ]
+queriesRunningFor600CriticalLimit=$(echo "$criticalLimits" | cut -d , -f 5 -s)
+queriesRunningFor600WarningLimit=$(echo "$warningLimits" | cut -d , -f 5 -s)
+if [ "$queriesRunningFor600CriticalLimit" ] && [ $queriesRunningFor600 -ge $queriesRunningFor600CriticalLimit ]
 	then
-	criticalString=$criticalString"$queriesRunningFor10Minutes queries running for 10 minutes reached 3; "
-elif [ $queriesRunningFor10Minutes -ge 1 ]
+	criticalString=$criticalString"$queriesRunningFor600 queries running for 10 minutes reached $queriesRunningFor600CriticalLimit; "
+elif [ "$queriesRunningFor600WarningLimit" ] && [ $queriesRunningFor600 -ge $queriesRunningFor600WarningLimit ]
 	then
-	warningString=$warningString"$queriesRunningFor10Minutes queries running for 10 minutes reached 1; "
+	warningString=$warningString"$queriesRunningFor600 queries running for 10 minutes reached $queriesRunningFor600WarningLimit; "
 fi
-performanceData=$performanceData"queriesRunningFor10Minutes=$queriesRunningFor10Minutes;1;3 "
+performanceData=$performanceData"queriesRunningFor600=$queriesRunningFor600;$queriesRunningFor600WarningLimit;$queriesRunningFor600CriticalLimit "
 
-if [ $queriesRunningForAnHour -ge 1 ]
+queriesRunningFor3600CriticalLimit=$(echo "$criticalLimits" | cut -d , -f 6 -s)
+queriesRunningFor3600WarningLimit=$(echo "$warningLimits" | cut -d , -f 6 -s)
+if [ "$queriesRunningFor3600CriticalLimit" ] && [ $queriesRunningFor3600 -ge $queriesRunningFor3600CriticalLimit ]
 	then
-	criticalString=$criticalString"$queriesRunningForAnHour queries running for an hour reached 1; "
+	criticalString=$criticalString"$queriesRunningFor3600 queries running for an hour reached $queriesRunningFor3600CriticalLimit; "
+elif [ "$queriesRunningFor3600WarningLimit" ] && [ $queriesRunningFor3600 -ge $queriesRunningFor3600WarningLimit ]
+	then
+	warningString=$warningString"$queriesRunningFor3600 queries running for an hour reached $queriesRunningFor3600WarningLimit; "
 fi
-performanceData=$performanceData"queriesRunningForAnHour=$queriesRunningForAnHour;;1 "
+performanceData=$performanceData"queriesRunningFor3600=$queriesRunningFor3600;$queriesRunningFor3600WarningLimit;$queriesRunningFor3600CriticalLimit "
 
 if [ $longestQueryTime -gt 0 ]
 	then
