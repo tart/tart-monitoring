938440116de7e75a78de36a2ca18402ba14248b3
Emre Hasegeli <hasegeli@tart.com.tr>
2013-12-01 19:20:56 +0200

    Change coding style of checkMongoDBOperations.py.

--- checkMongoDBOperations.py
+++ checkMongoDBOperations.py
@@ -3,7 +3,7 @@
 ##
 # Tart Monitoring
 #
-# Copyright (c) 2013, Tart İnternet Teknolojileri Ticaret AŞ
+# Copyright(c) 2013, Tart İnternet Teknolojileri Ticaret AŞ
 #
 # Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
 # granted, provided that the above copyright notice and this permission notice appear in all copies.
@@ -15,81 +15,77 @@
 # performance of this software.
 ##
 
-'''Imports for Python 3 compatibility.'''
 from __future__ import print_function
-try:
-    import __builtin__
-    __builtin__.input = __builtin__.raw_input
-except ImportError: pass
+
+import sys
+import signal
+import pymongo
+import argparse
 
 class Server:
     '''Connects to the MongoDB server, executes operations.'''
 
-    def __init__ (self, address, port):
-        import pymongo
-        self.__connection = pymongo.Connection (address, port)
+    def __init__(self, address, port):
+        self.__connection = pymongo.Connection(address, port)
 
-    def currentOperationCount (self):
+    def currentOperationCount(self):
         '''Execute currentOp operation on the server. Return count of operations.'''
-        return len (self.__connection.admin.current_op () ['inprog'])
+        return len(self.__connection.admin.current_op()['inprog'])
 
-class Timeout (Exception): pass
+class Timeout(Exception): pass
 
-def timeoutRaiser (signum, frame):
+def timeoutRaiser(signum, frame):
         raise Timeout
 
 class Checker:
     '''Checks operations on the MongoDB server. Returns warning or critical if given limits exceeded.'''
 
-    def parseArguments (self):
+    def parseArguments(self):
         '''Create ArgumentParser instance. Return parsed arguments.'''
-        from argparse import ArgumentParser, RawTextHelpFormatter, ArgumentDefaultsHelpFormatter
-        class Formatter (RawTextHelpFormatter, ArgumentDefaultsHelpFormatter): pass
-        argumentParser = ArgumentParser (formatter_class = Formatter, description = self.__doc__)
-        argumentParser.add_argument ('-H', '--host', dest = 'host', help = 'hostname', default = 'localhost')
-        argumentParser.add_argument ('-P', '--port', type = int, dest = 'port', default = 27017)
-        argumentParser.add_argument ('-w', '--warning', type = int, dest = 'warning', help = 'warning limit')
-        argumentParser.add_argument ('-c', '--critical', type = int, dest = 'critical', help = 'critical limit')
-        return argumentParser.parse_args ()
+        class Formatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter): pass
+        argumentParser = argparse.ArgumentParser(formatter_class=Formatter, description=self.__doc__)
+        argumentParser.add_argument('-H', '--host', dest='host', help='hostname', default='localhost')
+        argumentParser.add_argument('-P', '--port', type=int, dest='port', default=27017)
+        argumentParser.add_argument('-w', '--warning', type=int, dest='warning', help='warning limit')
+        argumentParser.add_argument('-c', '--critical', type=int, dest='critical', help='critical limit')
+        return argumentParser.parse_args()
 
-    def __init__ (self):
-        import sys
-        import signal
-        signal.signal (signal.SIGALRM, timeoutRaiser)
-        signal.alarm (10)
-        arguments = self.parseArguments ()
+    def __init__(self):
+        signal.signal(signal.SIGALRM, timeoutRaiser)
+        signal.alarm(10)
+        arguments = self.parseArguments()
         assert arguments.warning is None or arguments.critical is None or arguments.warning < arguments.critical
         self.__host = arguments.host
         self.__port = arguments.port
         self.__warningLimit = arguments.warning
         self.__criticalLimit = arguments.critical
 
-    def checkOperationCount (self):
+    def checkOperationCount(self):
         '''Run the main program.'''
-        print ('CheckMongoDBConnections ', end = '')
+        print('CheckMongoDBConnections ', end='')
         try:
-            server = Server (self.__host, self.__port)
-            currentOperationCount = server.currentOperationCount ()
+            server = Server(self.__host, self.__port)
+            currentOperationCount = server.currentOperationCount()
         except Exception as exception:
             try:
-                print ('unknown: ' + exception [1])
+                print('unknown: ' + exception[1])
             except IndexError:
-                print ('unknown: ' + str (exception))
+                print('unknown: ' + str(exception))
             returnValue = 3
         else:
             if self.__criticalLimit is not None and currentOperationCount > self.__criticalLimit:
-                print ('critical: ', end = '')
+                print('critical: ', end=' ')
                 returnValue = 2
             elif self.__warningLimit is not None and currentOperationCount > self.__warningLimit:
-                print ('warning: ', end = '')
+                print('warning:', end=' ')
                 returnValue = 1
             else:
-                print ('ok: ', end = '')
+                print('ok:', end=' ')
                 returnValue = 0
-            print (str (currentOperationCount) + ' operations ', end = '')
-            print ('| operationCount=' + str (currentOperationCount) + ';' + str (self.__warningLimit or '') + ';' + str (self.__criticalLimit or '') + ';0;')
+            print(str(currentOperationCount) + ' operations', end=' ')
+            print('| operationCount=' + str(currentOperationCount) + ';' + str(self.__warningLimit or '') + ';' + str(self.__criticalLimit or '') + ';0;')
         return returnValue
 
 if __name__ == '__main__':
-    checker = Checker ()
-    exit (checker.checkOperationCount ())
+    checker = Checker()
+    exit(checker.checkOperationCount())
