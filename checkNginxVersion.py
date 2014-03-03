2654e1e53fc3dbbf0b2450912aeb407438be6bc9
Emre Hasegeli <hasegeli@tart.com.tr>
2014-03-03 19:21:38 +0200

    Return 3 for exceptions on checkNginxVersion.py.

--- checkNginxVersion.py
+++ checkNginxVersion.py
@@ -18,76 +18,85 @@
 import sys
 import urllib2
 
-if len(sys.argv) < 2:
-    raise Exception('No argument.')
-if len(sys.argv) > 2:
-    raise Exception('Too many arguments.')
+def main():
+    if len(sys.argv) < 2:
+        raise Exception('No argument.')
+    if len(sys.argv) > 2:
+        raise Exception('Too many arguments.')
 
-response = None
-try:
-    response = urllib2.urlopen(sys.argv[1])
-except urllib2.HTTPError as error:
-    response = error
+    response = None
+    try:
+        response = urllib2.urlopen(sys.argv[1])
+    except urllib2.HTTPError as error:
+        response = error
 
-if not response:
-    raise Exception('No response.')
+    if not response:
+        raise Exception('No response.')
 
-info = response.info()
+    info = response.info()
 
-def getHeaderValue(info, key):
-    for header in info.headers:
-        if header.startswith(key + ':'):
-            return header[(len(key) + 1):]
+    def getHeaderValue(info, key):
+        for header in info.headers:
+            if header.startswith(key + ':'):
+                return header[(len(key) + 1):]
 
-server = getHeaderValue(info, 'Server')
+    server = getHeaderValue(info, 'Server')
 
-if not server:
-    raise Exception('No server header.')
+    if not server:
+        raise Exception('No server header.')
 
-split = server.strip().split('/')
+    split = server.strip().split('/')
 
-if split[0] != 'nginx':
-    raise Exception('Server is not Nginx.')
+    if split[0] != 'nginx':
+        raise Exception('Server is not Nginx.')
 
-if len(split) < 2:
-    raise Exception('No version on the server header.')
+    if len(split) < 2:
+        raise Exception('No version on the server header.')
 
-currentVersion = split[1]
+    currentVersion = split[1]
 
-response = urllib2.urlopen('http://nginx.org/en/download.html')
+    response = urllib2.urlopen('http://nginx.org/en/download.html')
 
-from HTMLParser import HTMLParser
+    from HTMLParser import HTMLParser
 
-class NginxDownloadHTMLParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.__lastTag = None
-        self.__h4 = None
-        self.__stableVersion = None
+    class NginxDownloadHTMLParser(HTMLParser):
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.__lastTag = None
+            self.__h4 = None
+            self.__stableVersion = None
 
-    def handle_starttag(self, tag, attrs):
-        self.__lastTag = tag
+        def handle_starttag(self, tag, attrs):
+            self.__lastTag = tag
 
-    def handle_data(self, data):
-        if self.__lastTag == 'h4':
-            self.__h4 = data
+        def handle_data(self, data):
+            if self.__lastTag == 'h4':
+                self.__h4 = data
 
-        if self.__h4 == 'Stable version' and data[:6] == 'nginx-':
-            self.__stableVersion = data[6:]
+            if self.__h4 == 'Stable version' and data[:6] == 'nginx-':
+                self.__stableVersion = data[6:]
 
-    def stableVersion(self):
-        return self.__stableVersion
+        def stableVersion(self):
+            return self.__stableVersion
 
-parser = NginxDownloadHTMLParser()
-parser.feed(response.read())
-stableVersion = parser.stableVersion()
+    parser = NginxDownloadHTMLParser()
+    parser.feed(response.read())
+    stableVersion = parser.stableVersion()
 
-if not stableVersion:
-    raise Exception('Could not get the stable version.')
+    if not stableVersion:
+        raise Exception('Could not get the stable version.')
 
-print('Current version ' + currentVersion + ' stable version: ' + stableVersion)
+    print('Current version ' + currentVersion + ' stable version: ' + stableVersion)
 
-if currentVersion != stableVersion:
-    sys.exit(1)
-sys.exit(0)
+    if currentVersion != stableVersion:
+        return False
+    return True
 
+if __name__ == '__main__':
+    try:
+        if main():
+            sys.exit(0)
+        sys.exit(1)
+    except Exception as exception:
+        print('Exception: ' + str(exception))
+        sys.exit(3)
